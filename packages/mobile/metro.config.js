7b0855b525434870ee91d0b3c5f3ec179c0b31b1
koko57 <aa@MacBook-Pro.local>
2023-12-28 20:13:51 +0100

    feat: setup for ios and android

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,13 +1,6 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
-const path = require("path");
-
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
 const config = {
     resolver: {
         unstable_enableSymlinks: true,
